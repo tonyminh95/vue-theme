b1fa688297bb6c67dff64d51949ee050aaf90fbb
tonyminh95 <minhbui12121995@gmail.com>
2019-06-01 08:24:29 +0900

    setup ui

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import router from './router/index'
 
+import PaperDashboard from "./plugins/paperDashboard";
+import "vue-notifyjs/themes/default.css";
+
+Vue.use(PaperDashboard);
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
